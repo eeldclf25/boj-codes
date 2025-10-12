83054504d0d2d73706597f3b086320544858c646
eeldclf25 <eeldclf25@gmail.com>
2025-10-12 21:32:34 +0900

    update

---
+++ boj_python/1260.py
@@ -0,0 +1,8 @@
+import sys
+
+def func(inputValue):
+    
+
+if __name__ == '__main__':
+    inputValue = sys.stdin.read().splitlines()
+    func(inputValue)
--- boj_python/5639.py
+++ boj_python/5639.py
@@ -1,44 +1,26 @@
 import sys
-from collections import deque
+sys.setrecursionlimit(10100)
 
 def func(inputValue):
-    rootNode = int(inputValue[0])
-    frontTree = {}
-    frontTree[rootNode] = [None, None]
-    for i in range(1, len(inputValue)):
-        value = int(inputValue[i])
-        currentNode = rootNode
-        while True:
-            if frontTree[currentNode][0] != None and currentNode > value:
-                currentNode = frontTree[currentNode][0]
-            if frontTree[currentNode][1] != None and currentNode < value:
-                currentNode = frontTree[currentNode][1]
-            if frontTree[currentNode][0] == None and currentNode > value:
-                frontTree[currentNode][0] = value
-                frontTree[value] = [None, None]
-                break
-            if frontTree[currentNode][1] == None and currentNode < value:
-                frontTree[currentNode][1] = value
-                frontTree[value] = [None, None]
-                break
-
-    endResult = []
-    endSearch = deque()
-    endSearch.append([int(inputValue[0]), 0])
-    while len(endSearch) != 0:
-        node, check = endSearch.pop()
-        if check == 1:
-            endResult.append(node)
-            continue
-        left, right = frontTree[node]
-        if check == 0:
-            endSearch.append([node, 1])
+    rootNode = inputValue[0]
+    left = None
+    right = None
+    for i in range(0, len(inputValue)):
+        if left == None and rootNode > inputValue[i]:
+            left = i
+        if right == None and rootNode < inputValue[i]:
+            right = i
+    
+    if left != None:
         if right != None:
-            endSearch.append([right, 0])
-        if left != None:
-            endSearch.append([left, 0])
-    print('\n'.join(map(str, endResult)))
+            func(inputValue[left:right])
+        else:
+            func(inputValue[left:len(inputValue)])
+    if right != None:
+        func(inputValue[right:len(inputValue)])
+    
+    print(rootNode)
 
 if __name__ == '__main__':
     inputValue = sys.stdin.read().splitlines()
-    func(inputValue)
+    func(list(map(int, inputValue)))
